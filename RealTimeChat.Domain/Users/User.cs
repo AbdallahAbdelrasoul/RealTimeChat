2466afd26d45c418ad26c358a0e43d906d4b0d21
Abdallah Abdelrasoul <abdallah.abdelrasoul@applogica.com>
2024-10-10 00:38:46 +0300

    - init User Domain. - inti ChatDbContext.

---
+++ RealTimeChat.Domain/Users/User.cs
@@ -0,0 +1,18 @@
+﻿namespace RealTimeChat.Domain.Users;
+
+public class User
+{
+    private User()
+    {
+        UserName = string.Empty;
+        Email = string.Empty;
+    }
+
+    public int Id { get; private set; }
+    public string UserName { get; private set; }
+    public string Email { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public bool IsOnline { get; private set; }
+    public DateTime LastSeen { get; private set; }
+
+}
