c6214f2b8f831c00491fc1ae3b77460c6d3c1619
Abdallah Abdelrasoul <abdallah.abdelrasoul@applogica.com>
2024-10-14 00:55:41 +0300

    # Authenticate SignalR Reuqests. - Get token at signalR Request.Query.

---
+++ RealTimeChat.Presentation/Hubs/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RealTimeChat.Presentation.Hubs
+{
+
+    //[Authorize]
+    public class ChatHub : Hub
+    {
+        //private readonly IMessagesService _messagesService;
+
+        //public ChatHub(IMessagesService messagesService)
+        //{
+        //    _messagesService = messagesService;
+        //}
+
+        public async Task SendMessage(string message)
+        {
+            var username = Context.User?.Identity?.Name;
+            await Clients.Client("a").SendAsync("ReceiveMessage", message);
+        }
+
+    }
+
+}
